9bf6ac6fb1224f4df74a2f8202c71866efeb806c
Gustavo Hoirisch <guga@maurorisch.com.br>
2017-09-29 13:01:53 +1000

    check db before add/remove and validate users and channels

--- plugins/at_here.py
+++ plugins/at_here.py
@@ -2,7 +2,6 @@
 This module adds responses to @here and @channel
 """
 import logging
-import re
 from slackbot.bot import listen_to, respond_to
 from model.database import SESSION
 from model.channel import Channel
@@ -25,78 +24,57 @@ def at_here(message, at_symbol=None):
         return
 
     at_here_log(at_symbol, channel, message.body['text'], user, "NO_SKIP")
-    try:
-        message.react('police-gus')
-        message.reply(
-            "Please don't use _{}_. Check out our Slacktiquete: \
-            https://myob.slack.com/archives/C3F2M5NFP/p1498438403848842".format(at_symbol),
-            in_thread=True
-        )
-    except:
-        pass
+    message.react('police-gus')
+    message.reply(
+        "Please don't use _{}_. Check out our Slacktiquete: \
+        https://myob.slack.com/archives/C3F2M5NFP/p1498438403848842".format(at_symbol),
+        in_thread=True
+    )
 
 @respond_to('^list$')
 def list_users(msg):
-    """List all users in DB"""
+    """
+    List all whitelist rules
+    Usage: `list`
+    """
     LOGGER.info("LIST :: %s", msg.body['user'])
     reply = "```\n"
-    reply += "{:^20} | {:^20}\n".format("User", "Channel")
-    reply += "======================\n"
-    for chan in Channel.query.all():
+    for chan in Channel.query.order_by(Channel.user_id.asc()).all():
         reply += "{0.user_id:^20} | {0.channel_name:^20}\n".format(chan)
     reply += "```"
     msg.reply(reply)
 
-@respond_to('^add (.*) to (.*)')
-def add_user(msg, user=None, channel=None):
-    """ Adding a new whitelist rule """
-    if user is None or channel is None:
-        msg.reply("Usage: `add @user to #channel`")
-        return
-
-    LOGGER.info("ADD :: %s :: %s to %s ", msg.body['user'], user, channel)
-    match = parse_channel_identifier(channel)
-    if not match:
-        LOGGER.info("ADD :: Bailing because failed to parse channel name and id: %s", channel)
-        return
-
-    LOGGER.info("ADD :: PARSED :: id: '%s', name: '%s'", match.group(1), match.group(2))
-
-    SESSION().add(Channel(channel_id=match.group(1), channel_name=match.group(2), user_id=user))
-    SESSION().commit()
-    msg.reply("User {} added to {} whitelist list".format(user, channel))
-
-@respond_to('^remove (.*) from (.*)')
-def remove_user(msg, user=None, channel=None):
-    """ Removing user from whitelist """
-    if user is None or channel is None:
-        msg.reply("Usage: `remove @user from #channel`")
-        return
-
-    LOGGER.info("REMOVE :: %s :: %s from %s ", msg.body['user'], user, channel)
-    match = parse_channel_identifier(channel)
-    if not match:
-        LOGGER.info("REMOVE :: Bailing because failed to parse channel name and id: %s", channel)
-        return
-
-    LOGGER.info("REMOVE :: PARSED :: id: '%s', name: '%s'", match.group(1), match.group(2))
-    records = Channel.query.filter_by(channel_id=match.group(1), user_id=user).all()
-    if records is None:
-        msg.reply("unable to delete from whitelist")
-        return
-
-    for record in records:
-        SESSION().delete(record)
-    SESSION().commit()
-    msg.reply("User {} deleted from {} whitelist".format(user, channel))
-
-
-def parse_channel_identifier(channel):
+@respond_to(r'^add (<@U[A-Z0-9]+>) to <#(C[A-Z0-9]*?)\|([a-zA-Z0-9\-]*?)>')
+def add_user(msg, user, channel_id, channel_name):
     """
-    given a string like so "<#C04UB2PBB|ex-ea>",
-    parse the ID (C04UB2PBB) and name (ex-ea) out of it
+    Add a user to a channel whitelist
+    Usage: `add @user to #channel`
     """
-    return re.search(r"<#(C[A-Z0-9]*?)\|([a-zA-Z0-9\-]*?)>", channel)
+    LOGGER.info("ADD :: %s :: %s to %s ", msg.body['user'], user, channel_name)
+    records = Channel.query.filter_by(channel_id=channel_id, user_id=user).all()
+    if records:
+        msg.reply("Rule already exists.")
+        return
+
+    SESSION().add(Channel(channel_id=channel_id, channel_name=channel_name, user_id=user))
+    SESSION().commit()
+    msg.reply("User {} added to {} whitelist list".format(user, channel_name))
+
+@respond_to(r'^remove (<@U[A-Z0-9]+>) from <#(C[A-Z0-9]*?)\|([a-zA-Z0-9\-]*?)>')
+def remove_user(msg, user, channel_id, channel_name):
+    """
+    Remove a user from a channel whitelist
+    Usage: `remove @user from #channel`
+    """
+    LOGGER.info("REMOVE :: %s :: %s from %s ", msg.body['user'], user, channel_name)
+    record = Channel.query.filter_by(channel_id=channel_id, user_id=user).first()
+    if not record:
+        msg.reply("Could not find whitelist rule.")
+        return
+
+    SESSION().delete(record)
+    SESSION().commit()
+    msg.reply("User {} deleted from {} whitelist.".format(user, channel_name))
 
 def is_allowed(user_id, channel_id):
     """
